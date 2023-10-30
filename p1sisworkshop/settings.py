3d6b26f9b9ee68128cc803de7bd31eebace1624e
Manuel Herrera <mahupa25@gmail.com>
2023-10-30 16:16:41 -0500

    .

--- p1sisworkshop/settings.py
+++ p1sisworkshop/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-
-STATIC_URL = 'static/'
-LOGIN_URL = '/signin'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+LOGIN_URL = '/signin'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
